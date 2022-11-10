368676877598602fa1a2928e61b95d31d390a690
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-11-10 18:26:23 +0530

    package and version setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memories</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates necessitatibus debitis, sit eos minima alias sapiente nam quas quos totam reprehenderit magnam saepe quam nulla, molestiae vitae neque explicabo. Inventore, necessitatibus! Facilis nisi alias facere?</p>
     </div>
   );
 }
