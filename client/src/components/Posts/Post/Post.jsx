6eccf5facb966333f0001e2954ed7ebf7253f020
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-11-13 12:10:26 +0530

    Post design

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import useStyles from './styles'
-import { Card, CardActions, CardMedia, Typography } from '@material-ui/core';
-import { ThumbUpAlt, DeleteOutlined, MoreHoriz } from '@material-ui/icons'
+import { Card, CardActions, CardMedia, Typography, CardContent, Button } from '@material-ui/core';
+import { ThumbUpAlt, Delete, MoreHoriz } from '@material-ui/icons'
+import moment from 'moment'
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
-            <CardMedia />
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant='h6'>{post.creator}</Typography>
+                <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{ color: 'white' }} size='small' onClick={() => { }}>
+                    <MoreHoriz fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant='body2' color='textSecondary'>{post.message}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} gutterBottom variant='h5'></Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size='small' color='primary' onClick={() => { }}>
+                    <ThumbUpAlt fontSize='small' />
+                    Like
+                </Button>
+                <Button size='small' color='primary' onClick={() => { }}>
+                    <Delete fontSize='small' />
+                    Delete
+                </Button>
+            </CardActions>
         </Card>
     )
 }
