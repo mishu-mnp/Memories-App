746bc494bb4f8dfc8b3f58113fcb96daf69e9ebc
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-11-15 03:15:26 +0530

    mongo atlas url

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import mongoose from "mongoose";
 import postRouter from './routes/posts.js';
-
+import dotenv from 'dotenv'
 
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -16,8 +16,8 @@ app.use(cors())
 
 app.use('/posts', postRouter);
 
-const CONNECTION_URL = 'mongodb+srv://mishu23:mishucodes2023@cluster0.rooc0wr.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
 
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
